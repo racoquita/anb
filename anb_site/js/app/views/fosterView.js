c354485b88b396de39ae3055c63d74ddf0fcfb3b
Richard Stovall <rstovall@me.com>
2012-09-19 20:37:49 -0400

    Working on the form and the form to email php - the php isn't done yet

--- anb_site/js/app/views/fosterView.js
+++ anb_site/js/app/views/fosterView.js
@@ -19,13 +19,22 @@ define(function (require) {
         },
 
         events: {
-            "click #choose a.blue_button" : "changeForm"
+            "change input[type='radio']" : "changeForm",
+            "focus input[type='text']" : "clearField",
+            "click #submit" : "submitForm"
         },
 
         changeForm: function(e){
-            e.preventDefault();
-            
-            $("#choose a.blue_button").toggleClass("selected");
+            e.target.value == 'foster' ? $('#petname_label').text('The pet I want to foster is') : $('#petname_label').text('The pet I want to adopt is');            
+        },
+
+        clearField: function(e){
+            $(e.target).attr('value','');
+        },
+
+        submitForm: function(e){
+            /*e.preventDefault();*/
+            console.log($(e.target));
         }
 	});
 
