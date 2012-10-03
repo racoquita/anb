2a9dc9cf97d68b3496bb14ed778bf0423f521224
Richard Stovall <rstovall@me.com>
2012-10-03 18:35:11 -0400

    stuff

--- anb_site/js/app/models/petModel.js
+++ anb_site/js/app/models/petModel.js
@@ -26,31 +26,32 @@ define(function (require){
     	},
         
     	parse: function(data){
+
             var new_desc, update = data.lastUpdate.$t.replace(/T.*$/,''), update = update.split('-');
             var a = moment([2012, 9, 27]), b = moment([update[0], update[1], update[2]]), test = b.diff(a);
 
             test < 0 ? 
-                new_desc = $(data.description.substr(0, data.description.lastIndexOf('<center>'))).text() + helper.getDonate() : 
-                new_desc = data.description;
+                new_desc = $(data.description.$t.substr(0, data.description.$t.lastIndexOf('<center>'))).text() + helper.getDonate() : 
+                new_desc = data.description.$t;
 
     		obj = {
-    			age: data.age,
-    			animal: data.animal,
-                description : new_desc,
-    			id: data.id,
-    			name: data.name,
-    			sex: data.sex == 'M' ? 'Male' : 'Female',
-    			size: data.size,
-    			email: data.contact.email,
-    			breeds: data.breeds.breed,
-                mix: data.mix,
-                contact: data.contact,
-                options: data.options
+    			age: data.age.$t,
+    			animal: data.animal.$t,
+                description : new_desc.$t,
+    			id: data.id.$t,
+    			name: data.name.$t,
+    			sex: data.sex.$t == 'M' ? 'Male' : 'Female',
+    			size: data.size.$t,
+    			email: data.contact.email.$t,
+    			breeds: data.breeds.breed.$t,
+                mix: data.mix.$t,
+                contact: data.contact.$t,
+                options: data.options.$t
     		}
 
     		if(data.media.photos) {
-    			obj.photo = data.media.photos.photo[0];
-                obj.photos = this.uniquePhotosX(obj.photo, data.media.photos);
+    			obj.photo = data.media.photos.photo[0].$t;
+                obj.photos = this.uniquePhotosX(obj.photo, data.media.photos.$t);
     		}
     		
     		return obj;
--- anb_site/js/app/views/petsView.js
+++ anb_site/js/app/views/petsView.js
@@ -88,7 +88,7 @@ define(function (require) {
                 
                 arg = arguments[1]
                 _.each(item, function(attr){
-                    console.log(attr);
+                   // console.log(attr);
 
                     var option = $("<button/>", {
                         id: attr.toLowerCase(),
